52e7fdd924145e89f0410f7e1631e3bbe03a0d5e
andidarusman <andidarusman92@gmail.com>
2019-02-05 19:05:16 +0700

    versi 1.0.0

---
+++ flaskr/__init__.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    #buat dan konfigurasi app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+            SECRET_KEY='dev',
+            DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+            )
+    
+    if test_config is None:
+        #meload config instance, bila ada tidak testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        #meload test config jika pass
+        app.config.from_mapping(test_config)
+
+    #memastikan folder instance ada
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # halaman sederhana say hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello Cakkk Ningg !!!'
+
+    return app
+
